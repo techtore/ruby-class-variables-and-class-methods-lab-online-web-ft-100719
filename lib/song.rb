861a8958cb8d113944d4078a7aa5ac95baab9153
Victoria Hale <vic.hale74@gmail.com>
2019-10-16 18:14:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
-    @@genre_count[genre] 
+    @@genre_count[genre] += 1
     @@genre_count[genre]  
   end
   end
